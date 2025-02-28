044a6a0a6ec669b2ca6bed929ea7b44b3a831f10
212111 <212111@1305-013>
2025-02-28 15:15:37 +0300

    Test

---
+++ LatypovDinar/LatypovDinar/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using LatypovDinar.Databases;
+using System.Windows;
+
+namespace LatypovDinar
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static DemoDemoExzBaseEntities db = new DemoDemoExzBaseEntities();
+    }
+}
---
+++ LatypovDinar/LatypovDinar/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using LatypovDinar.Pages;
+using System.IO;
+using System.Windows;
+
+namespace LatypovDinar
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Navigation.Initialize(this);
+            Navigation.NextPage(new MainPage());
+            Dinar();
+        }
+
+        private void Dinar()
+        {
+            string path = @"C:\Users\212111\Desktop\Демо\КОД 1.2._ВАРИАНТ_7\Сессия 1\agents_import";
+
+            foreach(var a in App.db.Agent)
+            {
+                if(a.Logo != "" && a.Logo != null)
+                {
+                    a.LogoPhoto = File.ReadAllBytes(path + a.Logo);
+                }
+                
+            }
+            App.db.SaveChanges();
+        }
+    }
+}
